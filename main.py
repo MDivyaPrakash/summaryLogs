e7a457f626d952dd2e0850c2e69db50020f8d92d
dippu94 <dippuprakash@gmail.com>
2024-03-31 17:32:20 -0400

    SecondCommit

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
-    return 'My Gradio app is running at /ui', 200
+    return 'The Gradio app is running at /ui', 200
+
+## End point to submit questions and documents
+@app.post('/submit_question_and_documents')
+async def root():
+    return 'Reached the post endpoint', 200
+
+## End point to handle the retrieval tasks
+@app.post('/get_question_and_facts')
+async def root():
+    return 'Reached the get Qns endpoint', 200
 
 app = gr.mount_gradio_app(app, demo, path='/ui')

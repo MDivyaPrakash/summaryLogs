91bd34152adc8799fe71558a005c9fc602114e76
dippu94 <dippuprakash@gmail.com>
2024-04-03 03:48:33 -0400

    UICommit

--- llm.py
+++ llm.py
@@ -21,14 +21,8 @@ class summaryWriter():
         url_list = sorted(urls.split(',')) if isinstance(urls, str) else sorted(urls)
 
         print(url_list)
-        CALL_LOGS = []
-        for urlVal in url_list:
-            CALL_LOG_DATE = urlVal.split("/")[-1].split("_")[2]
-            CALL_LOG = requests.get(urlVal).text
-            CALL_LOG = "\n".join([item.strip() for item in CALL_LOG.split("\n") if len(item.strip()) and not item.strip()[0].isdigit()])
-            CALL_LOGS.append((CALL_LOG_DATE, CALL_LOG))
-
-        response = self.get_response(self.question, CALL_LOGS)
+        call_logs = self.get_call_logs(url_list)
+        response = self.get_response(self.question, call_logs)
 
         return self.serve_UI_2(response,api_nd)
 
@@ -45,8 +39,18 @@ class summaryWriter():
         summary= output[-1]['generated_text'].split("[/INST]")[-1]
         return summary
     
+    def get_call_logs(self,url_list):
+        call_logs = []
+        for urlVal in url_list:
+            call_log_date = urlVal.split("/")[-1].split("_")[2]
+            call_log = requests.get(urlVal).text
+            call_log = "\n".join([item.strip() for item in call_log.split("\n") if len(item.strip()) and not item.strip()[0].isdigit()])
+            call_logs.append((call_log_date, call_log))
+
+        return call_logs
+    
     def query(self, payload):
-        json= {"inputs": payload, "parameters": {"max_new_tokens":256, "top_p":0.9, "temperature":0.75}}
+        json= {"inputs": payload, "parameters": {"max_new_tokens":256, "top_p":0.9, "temperature":0.6}}
         response = requests.post(API_URL, headers=HEADERS, json=json)
         return response.json()
     
@@ -59,6 +63,7 @@ class summaryWriter():
                 strVal = f"The facts at the end of the day {key} are below:" 
                 responseList.append(strVal)
                 responseList.append("\n".join(val))
+                break
         return self.question, "\n \n".join(responseList)
     
     def serve_UI_2(self,response,api_nd):
@@ -75,4 +80,5 @@ class summaryWriter():
                 strVal = f"The facts at the end of the day {key} are below:" 
                 responseList.append(strVal)
                 responseList.append("\n".join(val))
-        return "\n \n".join(responseList)
+        print(sorted(self.dateResponse.keys()))
+        return "\n \n".join(responseList),sorted(self.dateResponse.keys())
--- webapp.py
+++ webapp.py
@@ -6,34 +6,48 @@ from llm import *
 obj = summaryWriter()
 
 def document_addition_screen(question,urls):
-    output = obj.get_response_wrapper(urls,question,ui_nd=True,date=None)
-    return output
+    output,dates = obj.get_response_wrapper(urls,question,ui_nd=True,date=None)
+    return output,'\n'.join(dates)
 
 def qa_display_screen(date):
     question ,output = obj.get_response_wrapper(None,None,ui_nd=True,date=date)
     return question, output
 
-## Interface
-qa_screen = gr.Interface(
-    fn=qa_display_screen,
-    inputs=[gr.Textbox(label="Date (YYYY-MM-DD)")],
-    outputs=[gr.Textbox(label="Question"), gr.Textbox(label="Answer", lines = 10)],
-    title="Question and Answers Display Screen",
-    allow_flagging='auto'
-)
+def populate_slider(dates):
+    newDates = dates.split('\n')
+    new_slider = gr.Slider(value=1, minimum=1, maximum=len(newDates), step=1)
+    return new_slider,newDates[0]
 
-document_screen = gr.Interface(
-    fn=document_addition_screen,
-    inputs=[gr.Textbox(label="Question"),
-            gr.Textbox(label="URLs (separated by comma)")],
-    outputs=[gr.Textbox(label="Answer", lines = 10)],
-    title="Document Addition Screen",
-    allow_flagging='manual'
-)
+def populate_text_box(slider_value,dates):
+    newDates = dates.split('\n')
+    return newDates[slider_value-1]
 
-with gr.Blocks(theme=gr.themes.Base()) as demo:
-    gr.TabbedInterface([qa_screen, document_screen], ["QA Screen", "Document Screen"])
-    gr.themes.Base()
+with gr.Blocks() as demo:
+        
+        gr.Markdown( """  # The Summariser  """)
+
+        with gr.Tab("Document Addition Screen"):
+            with gr.Column():
+                qn_input = gr.Textbox(label="Question")
+                url_input = gr.Textbox(label="URLs (separated by comma)")
+                doc_submit = gr.Button("Submit")
+            text_output = gr.Textbox(label="Answer", lines = 10)
+            with gr.Accordion("Submitted Dates", open=False):
+                date_text = gr.Textbox(label="Dates",lines=5)
+        doc_submit.click(document_addition_screen, inputs=[qn_input,url_input], outputs=[text_output,date_text])
+
+        with gr.Tab("Question and Answers Display Screen"):
+            with gr.Column():
+                with gr.Row():
+                    data_slider = gr.Slider(label="Time Slider")
+                    date_text2 = gr.Textbox(label="Date") 
+                date_submit = gr.Button("Submit")
+            filter_question = gr.Textbox(label="Question")
+            filer_output = gr.Textbox(label="Answer", lines = 10)
+        
+        date_text.change(populate_slider,[date_text],[data_slider,date_text2])
+        data_slider.change(populate_text_box,[data_slider,date_text],[date_text2])
+        date_submit.click(qa_display_screen, inputs=date_text2, outputs=[filter_question,filer_output])
 
 if __name__ == "__main__":
     demo.queue().launch()
